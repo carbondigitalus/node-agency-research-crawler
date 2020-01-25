351473d04ce123ebd7fa8b5cb160a1814f9c43cf
Jared Ledbetter <jared@carbondigital.us>
2020-01-25 17:21:42 -0500

    ESLint index.js file

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var Crawler = require('crawler');
+const Crawler = require('crawler');
 
-var c = new Crawler({
+const c = new Crawler({
   maxConnections: 10,
   // This will be called for each crawled page
   callback: function(error, res, done) {
     if (error) {
       console.log(error);
     } else {
-      var $ = res.$;
+      const { $ } = res.$;
       // $ is Cheerio by default
       //a lean implementation of core jQuery designed specifically for the server
       console.log($('title').text());
