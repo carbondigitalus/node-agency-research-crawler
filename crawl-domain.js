2a6d6090cced5e4b988a0dc1bd2c90922f24a8e9
Jared Ledbetter <jared@carbondigital.us>
2020-01-26 20:32:23 -0500

    Finished combining the two projects

--- crawl-domain.js
+++ crawl-domain.js
@@ -1,5 +1,5 @@
 // core modules
-// const fs = require('fs');
+const fs = require('fs');
 const url = require('url');
 
 // third party modules
@@ -11,61 +11,70 @@ const request = require('request');
 const base = 'carbondigital.us';
 const firstLink = `https://${base}`;
 
-const crawled = [];
-const inboundLinks = [];
+// variables
+const json = {};
+
+// arrays
+json.links = [];
+json.crawled = [];
+json.inboundLinks = [];
 
 const makeRequest = function(crawlUrl, callback) {
   const startTime = new Date().getTime();
   request(crawlUrl, function(error, response, body) {
-    const pageObject = {};
-    pageObject.links = [];
-
     const endTime = new Date().getTime();
     const requestTime = endTime - startTime;
-    pageObject.requestTime = requestTime;
+    json.requestTime = requestTime;
 
     const $ = cheerio.load(body);
-    pageObject.title = $('title').text();
-    pageObject.url = crawlUrl;
+    json.title = $('title').text();
+    json.url = crawlUrl;
     $('a').each(function(i, elem) {
-      /*
-       insert some further checks if a link is:
-       * valid
-       * relative or absolute
-       * check out the url module of node: https://nodejs.org/dist/latest-v5.x/docs/api/url.html
-      */
-      pageObject.links.push({
+      json.links.push({
         linkText: $(elem).text(),
-        linkUrl: elem.attribs.href
+        linkUrl: $(elem).attr('href')
       });
     });
-    callback(error, pageObject);
+    callback(error, json);
   });
 };
 
 const myLoop = function(link) {
-  makeRequest(link, function(error, pageObject) {
-    console.log(pageObject);
-    crawled.push(pageObject.url);
+  makeRequest(link, function(error) {
+    // console.log(json);
+    json.crawled.push(json.url);
     async.eachSeries(
-      pageObject.links,
+      json.links,
       function(item, cb) {
-        const parsedUrl = url.URL(item.linkUrl);
+        const parsedUrl = item.linkUrl;
         // test if the url actually points to the same domain
         if (parsedUrl.hostname === base) {
           /*
          insert some further link error checking here
         */
-          inboundLinks.push(item.linkUrl);
+          json.inboundLinks.push(item.linkUrl);
         }
-        cb();
+        cb(
+          // To write to the system we will use the built in 'fs' library.
+          // In this example we will pass 3 parameters to the writeFile function
+          // Parameter 1 :  output.json - this is what the created filename will be called
+          // Parameter 2 :  JSON.stringify(json, null, 4) - the data to write, here we do an extra step by calling JSON.stringify to make our JSON easier to read
+          // Parameter 3 :  callback function - a callback function to let us know the status of our function
+          fs.writeFile('output.json', JSON.stringify(json, null, 4), function(
+            err
+          ) {
+            console.log(
+              'File successfully written! - Check your project directory for the output.json file'
+            );
+          })
+        );
       },
       function() {
-        const nextLink = _.difference(_.uniq(inboundLinks), crawled);
+        const nextLink = _.difference(_.uniq(json.inboundLinks), json.crawled);
         if (nextLink.length > 0) {
           myLoop(nextLink[0]);
         } else {
-          console.log('done!');
+          console.log('done crawling URL');
         }
       }
     );
